4de4fe84f8527369417c2c9425888dcd27043e4d
jdwfwbm <ted0426@aver.com>
2025-03-23 22:03:41 +0900

    초기 커밋.

---
+++ ImageProcessing/resource.h
@@ -0,0 +1,57 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ImageProcessing.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_ImageProcessingTYPE         130
+#define ID_WINDOW_MANAGER               131
+#define IDD_DIALOG1                     310
+#define IDD_DIALOG2                     312
+#define IDD_DIALOG3                     314
+#define IDD_DIALOG4                     316
+#define IDD_DIALOG5                     317
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define ID_DOWN_SAMPLING                32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+#define ID_32782                        32782
+#define ID_32783                        32783
+#define ID_32784                        32784
+#define ID_32785                        32785
+#define ID_UP_SAMPLING                  32786
+#define ID_QUANTIZATION                 32787
+#define ID_SUM_CONSTANT                 32788
+#define ID_SUB_CONSTANT                 32789
+#define ID_MUL_CONSTANT                 32790
+#define ID_DIV_CONSTANT                 32791
+#define ID_AND_OPERATE	                32792
+#define ID_OR_OPERATE                   32793
+#define ID_XOR_OPERATE                  32794
+#define ID_NOT_OPERATE                  32795
+#define ID_NEGA_TRANSFORM               32796
+#define ID_GAMMA_CORRECTION             32797
+#define ID_BINARIZATION                 32798
+#define ID_STRESS_TRANSFORM             32799
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        320
+#define _APS_NEXT_COMMAND_VALUE         32800
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
